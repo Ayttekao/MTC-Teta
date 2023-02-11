48ed773550c381b2848b4a1f69283cf67de983ad
Ayttekao <depish.huepish@yandex.ru>
2023-02-12 00:47:14 +0300

    Rename JSONEnricher

--- enrichment-task/src/main/java/io/ayttekao/enricher/JSONMessageEnricher.java
+++ enrichment-task/src/main/java/io/ayttekao/enricher/JSONMessageEnricher.java
@@ -1,6 +1,6 @@
 package io.ayttekao.enricher;
 
-public class JSONEnricher implements MessageEnricher {
+public class JSONMessageEnricher implements MessageEnricher {
     @Override
     public String enrich(String message) {
         return null;
