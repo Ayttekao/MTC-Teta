0aa38931bb4633fc2bc20d5e80e933ab6de85c19
Ayttekao <depish.huepish@yandex.ru>
2023-02-15 17:17:11 +0300

    Implement EnrichmentService

--- enrichment-task/src/main/java/io/ayttekao/service/EnrichmentService.java
+++ enrichment-task/src/main/java/io/ayttekao/service/EnrichmentService.java
@@ -1,21 +1,28 @@
 package io.ayttekao.service;
 
 import io.ayttekao.dao.ClientDao;
+import io.ayttekao.marshaller.MessageMarshaller;
 import io.ayttekao.model.Message;
 import io.ayttekao.validator.MessageValidator;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public class EnrichmentService {
+    private static final String ENRICHMENT_KEY = "enrichment";
     private final MessageValidator validator;
     private final ClientDao clientDao;
-
-    public EnrichmentService(MessageValidator validator, ClientDao clientDao) {
-        this.validator = validator;
-        this.clientDao = clientDao;
-    }
+    private final MessageMarshaller messageMarshaller;
 
     public String enrich(Message message) {
         if (validator.isValid(message)) {
-            return "Not implemented yet";
+            var marshalledMessageMap = messageMarshaller.marshall(message.getContent());
+            marshalledMessageMap.remove(ENRICHMENT_KEY);
+            var msisdn = marshalledMessageMap.get(message.getEnrichmentType().toString());
+            clientDao.findByMsisdn(Long.valueOf(msisdn.toString())).ifPresent(theUser -> {
+                marshalledMessageMap.put(ENRICHMENT_KEY, theUser);
+            });
+
+            return messageMarshaller.unmarshall(marshalledMessageMap);
         } else {
             return message.getContent();
         }
